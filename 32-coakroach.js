ecea324e54bfdf7629a97c8e20f49e360cbe4603
Fendy <xiangyap68@gmail.com>
2025-01-26 11:55:09 +0700

    32 Coakroach

---
+++ 32-coakroach.js
@@ -0,0 +1,11 @@
+// function cockroachSpeed(s) {
+//     //Good Luck!
+
+//     return Math.floor(s  * 100000 / 3600)
+// }
+
+const cockroachSpeed = (s) =>  Math.floor(s  * 100000 / 3600)
+
+
+
+console.log(cockroachSpeed(1.09))
