816b93c668a4efcf0e200fba7227180285dfb671
Fendy <76032813+fenry46@users.noreply.github.com>
2025-05-07 20:16:59 +0700

    print element with comma

---
+++ Printing-Array-elements-with-Comma-delimiters.js
@@ -0,0 +1,10 @@
+// function printArray(array) {
+//     //show me the code!
+//     return array.join()
+// }
+
+function printArray(array) {
+    return array;
+}
+
+console.log(printArray(["h", "o", "l", "a"]))
